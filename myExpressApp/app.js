35364bb1af1f9a93f913b030de2c50de90760048
AlexP999 <mikol999@gmail.com>
2021-06-25 19:10:45 +0300

    end

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -1,93 +1,3 @@
-// // ========================   l2   ===================
-
-// const express = require('express');
-// const expressHbs = require('express-handlebars'); //44:00
-// const path = require('path'); //46
-
-// const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true })); //1:12 научить нод читать бади??? читать формы
-
-// app.use(express.static(path.join(__dirname, 'static'))); //54 сделали публичную папку
-// app.set('view engine', '.hbs'); //49:00
-// app.engine(
-// 	'.hbs',
-// 	expressHbs({
-// 		defaultLayout: false
-// 	})
-// ); //49:00
-
-// app.set('views', path.join(__dirname, 'static')); //49:00
-
-// let users = [
-// 	{ name: 'Dima', age: 22, password: 'pass1' },
-// 	{ name: 'Vika', age: 18, password: 'pass1' },
-// 	{ name: 'Cvrystina', age: 16, password: 'pass1' },
-// 	{ name: 'Viktor', age: 25, password: 'pass1' }
-// ];
-// // app.get('/', (req, res) => {
-// // 	console.log(req);
-
-// // 	// res.write('Hellosiki \n');
-// // 	// res.write('Hellosiki2');
-
-// // 	// res.end('Helloo chat');
-// // 	// ++ 14:28 ++
-// // 	// res.send('HELO');
-// // 	// ++ 15:47 ++
-// // 	res.json('yakas hren');
-// // 	// чаще всего используется
-// // });
-
-// // app.get('/ping', (req, res) => {
-// // 	res.end('pong');
-// // });
-
-// // app.post('/', (req, res) => {
-// // 	console.log(req.body);
-// // 	res.json('POST');
-// // });
-// // 20:00 пост можно увидеть в postman. Пишем там в body json объект и чтобы получить в ноде добавляем 68 и app.use(express.json()) 49
-
-// // // +++ 25 +++
-// // app.get('/users', (req, res) => {
-// // 	res.json(users);
-// // });
-// // //тепер одного
-
-// // app.get('/users/1', (req, res) => {
-// // 	res.json(users[1]);
-// // });
-
-// // app.get('/users/:userId', (req, res) => {
-// // 	const { userId } = req.params;
-
-// // 	res.json(users[userId]);
-// // });
-// // +++ 29:00 +++ В одному реквести может быть params, query , body. req.query примає все що після ?, req.body - все що в бади и req.params  всі дінамічні параметри з урл
-
-// // +++   55  +++
-
-// app.get('/users', (req, res) => {
-// 	res.render('users', { name: 'Diana', users });
-// });
-
-// // +++   1:06   +++
-
-// app.get('/login', (req, res) => {
-// 	res.render('login', { isAdult: false }); //к if login.hbs
-// });
-
-// app.post('/login', (req, res) => {
-// 	console.log(req.body);
-// 	res.json('OK');
-// 	// +++ 1:14  +++ тре занайти юзера для пасс user.find()
-// });
-
-// app.listen(3000, () => {
-// 	console.log('App listen 3000');
-// });
-
 const express = require('express');
 const expressHbs = require('express-handlebars');
 const path = require('path');
@@ -99,20 +9,18 @@ app.use(express.urlencoded({ extended: true }));
 
 const userArrPath = path.join(__dirname, 'bd', 'usersArr.json');
 
-app.set('view engine', '.hbs'); //49:00
+app.set('view engine', '.hbs');
 app.engine(
 	'.hbs',
 	expressHbs({
 		defaultLayout: false
 	})
-); //49:00
-app.set('views', path.join(__dirname, 'static')); //49:00
+);
+app.set('views', path.join(__dirname, 'static'));
 
-app.use(express.static(path.join(__dirname, 'static'))); //54 сделали публичную папку
+app.use(express.static(path.join(__dirname, 'static')));
 
-// --------------------------UserData--------------------
-
-let newUser = { name: 'Jho', age: 24, password: 123 };
+let newUser = '';
 
 fs.readFile(userArrPath, (err, data) => {
 	if (err) {
@@ -121,22 +29,70 @@ fs.readFile(userArrPath, (err, data) => {
 	}
 	let users = JSON.parse(data);
 	console.log('users', users);
-	users.push(newUser);
-	fs.writeFile(userArrPath, JSON.stringify(users), (err) => {
-		if (err) {
-			console.log(err);
+	// ----------------------------------------------
+
+	// ----------  add new user and write to db function ----------
+	let addnewUser = (newUser) => {
+		users.push(newUser);
+		fs.writeFile(userArrPath, JSON.stringify(users), (err) => {
+			if (err) {
+				console.log(err);
+			}
+		});
+	};
+
+	// ----------------------------------------------
+
+	// --------------------------Pages--------------------
+	app.get('/', (req, res) => {
+		res.render('home');
+	});
+
+	app.get('/login', (req, res) => {
+		res.render('login');
+	});
+	app.get('/register', (req, res) => {
+		res.render('register');
+	});
+
+	app.get('/welcome', (req, res) => {
+		res.render('welcome', { pass: true });
+	});
+
+	app.post('/login', (req, res) => {
+		console.log('usersPost', users);
+
+		const sameName = users.find(({ name }) => name === req.body.name);
+
+		console.log('sameName', sameName);
+
+		if (!sameName) {
+			res.render('welcome', { pass: false });
+			return;
 		}
+		if (!(sameName.password === req.body.password)) {
+			res.render('welcome', { pass: false });
+			return;
+		}
+
+		res.render('welcome', { pass: true, sameName });
+	});
+
+	app.post('/register', (req, res) => {
+		let sameName = users.find(({ name }) => name === req.body.name);
+		if (sameName) {
+			res.json('Choose another name');
+			return;
+		}
+		newUser = req.body;
+		addnewUser(newUser);
+	});
+
+	app.get('/users', (req, res) => {
+		res.render('users', { users });
 	});
 });
-// --------------------------UserData--------------------
-
-// --------------------------Pages--------------------
-
-app.get('/login', (req, res) => {
-	res.render('login'); //к if login.hbs
-});
-
-// --------------------------Pages--------------------
+// ----------------------------------------------
 app.listen(3000, () => {
 	console.log('App listen 3000');
 });
