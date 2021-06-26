5ce60554ceb49ac4674b9f95567bb3ab5258e580
AlexP999 <mikol999@gmail.com>
2021-06-26 21:14:34 +0300

    end

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -21,77 +21,71 @@ app.set('views', path.join(__dirname, 'static'));
 app.use(express.static(path.join(__dirname, 'static')));
 
 let newUser = '';
+const users = JSON.parse(fs.readFileSync(userArrPath));
 
-fs.readFile(userArrPath, (err, data) => {
-	if (err) {
-		console.log(err);
+// ----------------------------------------------
+
+// ----------  add new user and write to db function ----------
+let addnewUser = (newUser) => {
+	users.push(newUser);
+	fs.writeFile(userArrPath, JSON.stringify(users), (err) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+};
+
+// ----------------------------------------------
+
+// --------------------------Pages--------------------
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.get('/login', (req, res) => {
+	res.render('login');
+});
+app.get('/register', (req, res) => {
+	res.render('register');
+});
+
+app.get('/welcome', (req, res) => {
+	res.render('welcome', { pass: true });
+});
+
+app.post('/login', (req, res) => {
+	console.log('usersPost', users);
+
+	const sameName = users.find(({ name }) => name === req.body.name);
+
+	console.log('sameName', sameName);
+
+	if (!sameName) {
+		res.render('welcome', { pass: false });
 		return;
 	}
-	let users = JSON.parse(data);
-	console.log('users', users);
-	// ----------------------------------------------
+	if (!(sameName.password === req.body.password)) {
+		res.render('welcome', { pass: false });
+		return;
+	}
 
-	// ----------  add new user and write to db function ----------
-	let addnewUser = (newUser) => {
-		users.push(newUser);
-		fs.writeFile(userArrPath, JSON.stringify(users), (err) => {
-			if (err) {
-				console.log(err);
-			}
-		});
-	};
-
-	// ----------------------------------------------
-
-	// --------------------------Pages--------------------
-	app.get('/', (req, res) => {
-		res.render('home');
-	});
-
-	app.get('/login', (req, res) => {
-		res.render('login');
-	});
-	app.get('/register', (req, res) => {
-		res.render('register');
-	});
-
-	app.get('/welcome', (req, res) => {
-		res.render('welcome', { pass: true });
-	});
-
-	app.post('/login', (req, res) => {
-		console.log('usersPost', users);
-
-		const sameName = users.find(({ name }) => name === req.body.name);
-
-		console.log('sameName', sameName);
-
-		if (!sameName) {
-			res.render('welcome', { pass: false });
-			return;
-		}
-		if (!(sameName.password === req.body.password)) {
-			res.render('welcome', { pass: false });
-			return;
-		}
-
-		res.render('welcome', { pass: true, sameName });
-	});
-
-	app.post('/register', (req, res) => {
-		let sameName = users.find(({ name }) => name === req.body.name);
-		if (sameName) {
-			res.json('Choose another name');
-			return;
-		}
-		newUser = req.body;
-		addnewUser(newUser);
-	});
-
-	app.get('/users', (req, res) => {
-		res.render('users', { users });
-	});
+	res.render('welcome', { pass: true, sameName });
 });
+
+app.post('/register', (req, res) => {
+	let sameName = users.find(({ name }) => name === req.body.name);
+	if (sameName) {
+		res.json('Choose another name');
+		return;
+	}
+	newUser = req.body;
+	addnewUser(newUser);
+});
+
+app.get('/users', (req, res) => {
+	res.render('users', { users });
+});
+
 // ----------------------------------------------
 app.listen(3000, () => {
 	console.log('App listen 3000');
