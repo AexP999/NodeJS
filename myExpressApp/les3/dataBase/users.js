c8a838ba6c441320bb4f6606260813a2908bff73
AlexP999 <mikol999@gmail.com>
2021-07-01 18:12:31 +0300

    1

--- myExpressApp/les3/dataBase/users.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-	{ name: 'Dima', age: '22', password: 'pass0' },
-	{ name: 'Vika', age: '18', password: 'pass1' },
-	{ name: 'Cvrystina', age: '16', password: 'pass2' },
-	{ name: 'Viktor', age: '25', password: 'pass3' },
-	{ name: 'Jho', age: '24', password: 'pass4' },
-	{ name: 'Dimaerfe', password: 'pass5' },
-	{ name: 'Jhonson', password: 'pass6' }
-];
